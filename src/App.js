2982ba4ce0bed6ba4a5fb279702b93f1f10f59c5
Viktor <zeninoid@list.ru>
2023-03-22 15:27:21 +0400

    1

--- src/App.js
+++ src/App.js
@@ -28,10 +28,9 @@ function Result({ correct }) {
     <div className="result">
       <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
       <h2>Вы отгадали {correct} ответа из {questions.length}</h2>
-      <a href="/">
-        <button>Попробовать снова</button>
-      </a>
-      
+        <a href="/">
+          <button>Попробовать снова</button>
+        </a>
     </div>
   );
 }
