cf38533fa88f029b61d80991f5f4e0e24da33c66
Viktor <zeninoid@list.ru>
2023-03-27 23:59:54 +0400

    Currency-Converter

--- src/App.js
+++ src/App.js
@@ -3,10 +3,45 @@ import { Block } from './Block';
 import './index.scss';
 
 function App() {
+  const [fromCurrency, setFromCurrency] = React.useState('AUD');
+  const [toCurrency, setToCurrency] = React.useState('USD');
+  const [fromPrice, setFromPrice] = React.useState(0);
+  const [toPrice, setToPrice] = React.useState(0);
+  const [rates, setRates] = React.useState({});
+
+  React.useEffect(() => {
+    fetch('https://www.cbr-xml-daily.ru/latest.js')
+      .then((res) => res.json())
+      .then((json) => { setRates(json.rates); console.log(json.rates); })
+      .catch((err) => {
+        console.warn(err); alert('Не удалось получить информацию');
+      })
+  }, [])
+
+  const onChangeFromPrice = (value) => {
+    const price = value / rates[fromCurrency];
+    const result = price * rates[toCurrency];
+    setToPrice(result.toFixed(2));
+    setFromPrice(value);
+  };
+  const onChangeToPrice = (value) => {
+    const result = (rates[fromCurrency] / rates[toCurrency]) * value;
+    setFromPrice(result.toFixed(2));
+    setToPrice(value);
+  };
+
+  React.useEffect(() => {
+    onChangeFromPrice(fromPrice);
+  }, [fromCurrency]);
+
+  React.useEffect(() => {
+    onChangeToPrice(toPrice);
+  }, [toCurrency]);
+  
   return (
     <div className="App">
-      <Block value={0} currency="RUB" onChangeCurrency={(cur) => console.log(cur)} />
-      <Block value={0} currency="USD" />
+      <Block value={fromPrice} currency={fromCurrency} onChangeCurrency={setFromCurrency} onChangeValue={onChangeFromPrice}/>
+      <Block value={toPrice} currency={toCurrency} onChangeCurrency={setToCurrency} onChangeValue={onChangeToPrice} />
     </div>
   );
 }
