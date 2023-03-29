561cfbb00ceb683838e0a8d9bd9ea81b1fa99b3c
Viktor <zeninoid@list.ru>
2023-03-29 15:51:07 +0400

    PhotoCollectionEnd

--- src/App.js
+++ src/App.js
@@ -15,35 +15,75 @@ function Collection({ name, images }) {
   );
 }
 
+const cats = [
+  { "name": "Все" },
+  { "name": "Море" },
+  { "name": "Горы" },
+  { "name": "Архитектура" },
+  { "name": "Города" }
+];
+
 function App() {
+
+  const [collections, setCollections] = React.useState([]);
+  const [searchValue, setSearchValue] = React.useState("");
+  const [categoryId, setCategoryId] = React.useState(0);
+  const [isLoading, setIsLoading] = React.useState(true);
+  const [page, setPage] = React.useState(1);
+
+  React.useEffect(() => {
+
+    const category = categoryId ? `category=${categoryId}` : '';
+    
+
+    setIsLoading(true);
+
+    fetch(`https://642412d2d6152a4d4805e540.mockapi.io/photo_collections?page=${page}&limit=3&${category}`)
+      .then((res) => res.json())
+      .then((json) => {
+        setCollections(json);
+      })
+      .catch((err) => {
+        console.warn(err);
+        alert('Ошибка при получении данных')
+      }).finally(() => setIsLoading(false))
+  }, [categoryId, page])
+
   return (
     <div className="App">
       <h1>Моя коллекция фотографий</h1>
       <div className="top">
         <ul className="tags">
-          <li className="active">Все</li>
-          <li>Горы</li>
-          <li>Море</li>
-          <li>Архитектура</li>
-          <li>Города</li>
+          {
+            cats.map((obj, i) => (<li onClick={() => setCategoryId(i)} className={categoryId === i ? "active" : ''} key={obj.name}>{obj.name}</li>))
+          }
         </ul>
-        <input className="search-input" placeholder="Поиск по названию" />
+        <input onChange={e => setSearchValue(e.target.value)} value={searchValue} className="search-input" placeholder="Поиск по названию" />
       </div>
       <div className="content">
-        <Collection
-          name="Путешествие по миру"
-          images={[
-            'https://images.unsplash.com/photo-1540959733332-eab4deabeeaf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-            'https://images.unsplash.com/photo-1560840067-ddcaeb7831d2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDB8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-            'https://images.unsplash.com/photo-1531219572328-a0171b4448a3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzl8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-            'https://images.unsplash.com/photo-1573108724029-4c46571d6490?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzR8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-          ]}
-        />
+        {isLoading ? (
+          <h2>Идёт загрузка...</h2>
+        ) : (
+
+          collections.filter((obj) =>
+            obj.name.toLowerCase().includes(searchValue.toLowerCase())
+          )
+            .map((obj, index) => (
+              <Collection
+                key={index}
+                name={obj.name}
+                images={obj.photos}
+              />
+
+            ))
+        )}
       </div>
       <ul className="pagination">
-        <li>1</li>
-        <li className="active">2</li>
-        <li>3</li>
+        {
+          [...Array(3)].map((_, i) => (
+            <li onClick={() => setPage(i + 1)} className={page === i + 1 ? "active" : ''}>{i + 1}</li>
+          ))
+        }
       </ul>
     </div>
   );
